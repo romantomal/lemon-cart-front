f7b8eee6b2cdffa2545f61e0149b1068b3b61237
RomanTomal <57354716+romantomal@users.noreply.github.com>
2021-04-12 20:16:39 +0300

    Fixed Navbar scroll. Fixed Navbar style to classic.
    Changed:
    - Navbar.tsx;
    - Navbar.module.scss;
    - CoreLayout.tsx;
    
    Added:
    *
    
    Removed:
    *

--- components/Navigation/Navbar.tsx
+++ components/Navigation/Navbar.tsx
@@ -6,12 +6,12 @@ import {useTypedSelector} from "../../core/hooks/useTypedSelector";
 
 const Navbar: React.FC = () => {
     const {isShowNavbar} = useTypedSelector(state => state.app)
-    const navbar = isShowNavbar ? styles.navbar : styles.navbarHide;
+    const navbar = isShowNavbar ? styles.navbar : `${styles.navbar} ${styles.hide}`;
 
     return (
         <div className={navbar}>
             <Image src={logo} alt="Lemon Logo" width={50} height={50}/>
-            <span className={styles.navbarTitle}>Lemon Cart</span>
+            <span className={styles.title}>Lemon Cart</span>
         </div>
     );
 }
