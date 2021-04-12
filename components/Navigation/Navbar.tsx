61dbcb643b3af5a3945ba4f8a11a09633aa4bc7b
RomanTomal <57354716+romantomal@users.noreply.github.com>
2021-04-12 20:40:33 +0300

    Move content from assets to public. Logo now returns to main page.
    Changed:
    - move logo.svg and loader.svg to new folder;
    - Loader.module.scss;
    - Navbar.tsx;
    - Navbar.module.scss;
    
    Added:
    *
    
    Removed:
    *

--- components/Navigation/Navbar.tsx
+++ components/Navigation/Navbar.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import Image from "next/image";
-import logo from "../../assets/images/logo.svg";
+import logo from "../../public/images/logo.svg";
 import styles from '../../styles/component/Navbar.module.scss';
 import {useTypedSelector} from "../../core/hooks/useTypedSelector";
+import {useRouter} from "next/router";
 
 const Navbar: React.FC = () => {
+    const router = useRouter()
     const {isShowNavbar} = useTypedSelector(state => state.app)
     const navbar = isShowNavbar ? styles.navbar : `${styles.navbar} ${styles.hide}`;
 
     return (
         <div className={navbar}>
-            <Image src={logo} alt="Lemon Logo" width={50} height={50}/>
+            <Image className={styles.logo} onClick={() => router.push('/')} src={logo} alt="Lemon Logo" width={50} height={50}/>
             <span className={styles.title}>Lemon Cart</span>
         </div>
     );
